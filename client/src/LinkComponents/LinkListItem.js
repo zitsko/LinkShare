a33ee5657835c8cb72ae07f0b553c11887ab70fa
zitsko <kostaskotsis87@gmail.com>
2023-07-25 17:40:05 +0300

    update platform rendering in browser

--- client/src/LinkComponents/LinkListItem.js
+++ client/src/LinkComponents/LinkListItem.js
@@ -4,9 +4,9 @@ import React from "react";
 function LinkListItem({ link, handleEditLink, handleDeleteLink }) {
   return (
     <div>
-      <p>Platform: {link.platform}</p>
-      {link.customPlatform && <p>Custom Platform: {link.customPlatform}</p>}
-      {/* <p>CustomPlatform: {link.customPlatform}</p> */}
+      {/* <p>Platform: {link.platform}</p>
+      {link.customPlatform && <p>Custom Platform: {link.customPlatform}</p>} */}
+      <p>Platform: {link.customPlatform || link.platform}</p>
       <p>URL: {link.url}</p>
       <button onClick={() => handleEditLink(link)}>Edit</button>
       <button onClick={() => handleDeleteLink(link._id)}>Delete</button>
