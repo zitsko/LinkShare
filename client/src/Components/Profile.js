5b25938f3073cfcebbfdead8972fb9430afc3fdc
zitsko <kostaskotsis87@gmail.com>
2023-07-26 22:10:24 +0300

    update

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -1,13 +1,118 @@
-import React from 'react';
-import MainNavbar from '../Navbars/MainNavbar';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import MainNavbar from "../Navbars/MainNavbar";
+import axios from "axios";
+import ProfileForm from "../ProfileComponents/ProfileForm";
 
-function Profile(){
-    return(
-        <div>
-            <MainNavbar />
-        <h1>Profile</h1>
-        </div>
-    )
+function Profile() {
+  const [firstName, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [profileEmail, setProfileEmail] = useState("");
+  const [profileDetails, setProfileDetails] = useState({});
+  const [user, setUser] = useState({
+    _id: "",
+    email: "",
+  });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      axios
+        .post("http://localhost:3636/user/verify", {
+          token: localStorage.getItem("token"),
+        })
+        .then(({ data }) => {
+          if (data.userData._id) {
+            console.log(data);
+            setUser(data.userData);
+            fetchProfileDetails(data.userData._id);
+          } else {
+            navigate("/");
+          }
+        })
+        .catch((error) => {
+          console.error("Error verifying user:", error);
+          navigate("/");
+        });
+    } else {
+      navigate("/");
+    }
+  }, []);
+
+  const fetchProfileDetails = async (userId) => {
+    try {
+      console.log("Fetching profile details for user:", userId);
+      const response = await axios.get(`http://localhost:3636/profile/${userId}`);
+      console.log("Response data:", response.data);
+      setProfileDetails(response.data); 
+      console.log("Profile details fetched:", profileDetails);
+    //   if (response.data.length > 0) {
+    //     // If response data object is not empty, it means the user has an existing profile.
+    //     // Set the state variables with the fetched profile data.
+    //     const profileData = response.data;
+    //     setName(profileData.firstName);
+    //     setLastName(profileData.lastName);
+    //     setProfileImage(profileData.profilePhoto);
+    //     setProfileEmail(profileData.email);
+    //   }
+    } catch (error) {
+      console.error("Error fetching profile details:", error);
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // const profileData = null
+    try {
+        const profileData = {
+        firstName: firstName,
+        lastName: lastName,
+        profilePhoto: profileImage,
+        profileEmail: profileEmail,
+        userId: user._id,
+      };
+      console.log("Profile Data:", profileData);
+      
+      if (profileDetails.length > 0) {
+        // If profileDetails array is not empty, it means the user has an existing profile.
+        // Get the _id from one of the profiles (you can choose the first one in this case).
+        profileData._id = profileDetails[0]._id;
+  
+        // Perform a PUT request to update the profile.
+        await axios.put(`http://localhost:3636/profile/${profileData._id}`, profileData);
+        console.log("Profile details updated successfully!");
+      } else {
+        // If profileDetails array is empty, it means the user does not have a profile yet.
+        // Perform a POST request to create a new profile.
+        await axios.post("http://localhost:3636/profile", profileData);
+        console.log("New profile created successfully!");
+      }
+    } catch (error) {
+      console.error("Error updating/creating profile details:", error);
+    }
+  };
+  
+
+  return (
+    <div>
+      <MainNavbar />
+      <h1>Profile Details</h1>
+
+      {/* Render ProfileForm to allow the user to edit and save the profile data */}
+      <ProfileForm
+        firstName={firstName}
+        lastName={lastName}
+        profileImage={profileImage}
+        profileEmail={profileEmail}
+        handleFirstNameChange={(e) => setName(e.target.value)}
+        handleLastNameChange={(e) => setLastName(e.target.value)}
+        handleProfileImageChange={(e) => setProfileImage(e.target.value)}
+        handleProfileEmailChange={(e) => setProfileEmail(e.target.value)}
+        handleSubmit={handleSubmit}
+      />
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
---
+++ client/src/ProfileComponents/ProfileForm.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+function ProfileForm({
+  profileImage,
+  firstName,
+  lastName,
+  profileEmail,
+  handleProfileImageChange,
+  handleFirstNameChange,
+  handleLastNameChange,
+  handleProfileEmailChange,
+  handleSubmit,
+}) {
+  return (
+    <form>
+      <div>
+        <label htmlFor="profileImage">Profile Image:</label>
+        <input
+          type="text"
+          id="profileImage"
+          name="profileImage"
+          value={profileImage}
+          onChange={handleProfileImageChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          value={firstName}
+          onChange={handleFirstNameChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={lastName}
+          onChange={handleLastNameChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="profileEmail">Email:</label>
+        <input
+          type="email"
+          id="profileEmail"
+          name="profileEmail"
+          value={profileEmail}
+          onChange={handleProfileEmailChange}
+        />
+      </div>
+      <button type="submit" onClick={handleSubmit}>
+        Save
+      </button>
+    </form>
+  );
+}
+
+export default ProfileForm;
+
