b961e70adffbadd137827766bdfa7d5e1b269fea
zitsko <kostaskotsis87@gmail.com>
2023-08-05 20:42:09 +0300

    updates

--- client/src/LinkComponents/LinkListItem.js
+++ client/src/LinkComponents/LinkListItem.js
@@ -3,13 +3,17 @@ import React from "react";
 
 function LinkListItem({ link, handleEditLink, handleDeleteLink }) {
   return (
-    <div>
-      {/* <p>Platform: {link.platform}</p>
-      {link.customPlatform && <p>Custom Platform: {link.customPlatform}</p>} */}
-      <p>Platform: {link.customPlatform || link.platform}</p>
-      <p>URL: {link.url}</p>
-      <button onClick={() => handleEditLink(link)} className="btn primary-btn">Edit</button>
-      <button onClick={() => handleDeleteLink(link._id)} className="btn no-background-intense-btn">Delete</button>
+    <div className="link-list-item-container">
+
+      <div>
+        <p>Platform: {link.customPlatform || link.platform}</p>
+        <p>URL: {link.url}</p>
+      </div>
+
+      <div className="edit-delete-btn-container">
+        <button onClick={() => handleEditLink(link)} className="btn primary-btn circle-btn">Edit</button>
+        <button onClick={() => handleDeleteLink(link._id)} className="btn no-background-intense-btn circle-btn">Delete</button>
+      </div>
     </div>
   );
 }
